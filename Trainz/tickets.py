48863ad9ad9e42d91e0e7025d6a14f7761124269
Jeslyn <jeslyn.sorotsky@gmail.com>
2025-11-07 11:03:06 -0500

    logic for table creation

--- Trainz/tickets.py
+++ Trainz/tickets.py
@@ -3,46 +3,35 @@
 
 #following tutorial mentioned in iteration3
 import sqlite3
-con = sqlite3.connect("trainz.db") #creates our trainz db
-cur = con.cursor()
 
-#Create ***UPDATE FOREIGN ONCE USER AND RESERVATION TABLES CREATED
-# cur.execute("PRAGMA foreign_keys = ON;") #sets foreign key
-cur.execute("""
+def init_tickets_table(): #if not created, init
+    con = sqlite3.connect("trainz.db")#creates our trainz db
+    cur=con.cursor()
+    #cur.execute("PRAGMA foreign_keys = ON;")
+    #Create ***UPDATE FOREIGN ONCE USER AND RESERVATION TABLES CREATED
+    cur.execute("""
 CREATE TABLE IF NOT EXISTS Tickets(
           ticket_id TEXT PRIMARY KEY,
           user_id TEXT NOT NULL,
           reservation_id TEXT NOT NULL)
           """) #USER ID AND RESERVATION ID MUST BE CREATED AS TABLES ALREADY--- THEY ARE FOREIGN KEYS. CAN'T CALL THEM FOREIGN KEYS IF TABLES AREN'T CREATED YET
+    #updated code with foreign: 
+    '''CREATE TABLE IF NOT EXISTS Tickets(
+        ticket_id TEXT PRIMARY KEY,
+        user_id TEXT NOT NULL,
+        reservation_id TEXT NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES Users(user_id),
+        FOREIGN KEY (reservation_id) REFERENCES Reservations(reservation_id)
+    )'''
+    con.commit()
+    con.close()
 
-#updated code with foreign:
-'''CREATE TABLE IF NOT EXISTS Tickets(
-    ticket_id TEXT PRIMARY KEY,
-    user_id TEXT NOT NULL,
-    reservation_id TEXT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    FOREIGN KEY (reservation_id) REFERENCES Reservations(reservation_id)
-)'''
-
-
-con.commit()
-con.close()
 
 def insert_ticket(ticket_id, user_id, reservation_id):
     con=sqlite3.connect("trainz.db")
     cur=con.cursor()
+    #cur.execute("PRAGMA foreign_keys = ON;")
     cur.execute("""INSERT INTO Tickets(ticket_id, user_id, reservation_id)
                 VALUES (?,?,?)""", (ticket_id, user_id, reservation_id))
     con.commit()
     con.close()
-
-
-def insert_ticket(ticket_id, user_id, reservation_id):
-    con = sqlite3.connect("trainz.db")
-    c=con.cursor()
-    cur.execute("""
-        INSERT INTO Tickets VALUES (?, ?, ?)
-    """, (ticket_id, user_id, reservation_id))
-    con.commit()
-    con.close()
-
