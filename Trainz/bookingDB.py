678e92254d7edea0c52729489d2da34af07acaf2
afkCYa <chenya17@hotmail.com>
2025-11-11 02:28:17 -0500

    function to fetch past/current trip/reservations for viewing added, not tested yet

--- Trainz/bookingDB.py
+++ Trainz/bookingDB.py
@@ -4,6 +4,7 @@ from trip import TripContainer
 from reservation import ReservationClass
 from ticket import Ticket 
 import tickets #sql
+import datetime
 
 class BookingDBClass:
 
@@ -12,6 +13,7 @@ class BookingDBClass:
 
     def create_trip(user_id): #create a unique trip id, create trip and add to bookingdb's list
         #each trip has a unique trip_id, let's randomize it
+        # why randomize it???
         trip_id = "T"+ str(random.randint(0,99999))
         trip = TripContainer(trip_id,user_id)
         BookingDBClass.trips_database.append(trip)
@@ -42,5 +44,88 @@ class BookingDBClass:
         # a ticket gets created and stored in memory
 
         ticket=BookingDBClass.create_ticket(user_id,reservation.reservation_id, ticket_id)
+
+        # need the reference to append to the trip
+        return trip.trip_id
+    
+    # should always work, to get the trip from id
+    def find_trip(trip_id):
+        for t in BookingDBClass.trips_database:
+            if t.trip_id == trip_id:
+                return t
+
+    # adding more members to an already created trip
+    def append_reservation(tripId, fname,lname,age,selected_option, user_id, date, current):
+        #create the trip object that will store the session's reservations
+        trip = BookingDBClass.find_trip(tripId)
+        #get the reservation object
+        reservation = ReservationClass(fname,lname,age,selected_option, date, current)
+        #store the reservation object in the newly created trip container
+        trip.add_reservation(reservation)
+        print("Trip container contains the reservation")
+        trip.add_trip_member(user_id)
+        
+        # a ticket gets created and stored in memory
+        ticket_id = "ticket-"+str(random.randint(0,99999))
+
+        # a ticket gets created and stored in memory
+
+        ticket=BookingDBClass.create_ticket(user_id,reservation.reservation_id, ticket_id)
+
+    # user enters lname and id, but only use id
+    def getReservationsFromUserId(userId):
+        tempAllTrips = []
+        tempAllTicketsResId = []
+        # get all trips of user, userId is a list
+        for trip in BookingDBClass.trips_database:
+            if userId in trip.user_id:
+                tempAllTrips.append(trip)
+
+        # user has no trips, abort
+        if not tempAllTrips:
+            return []
+        
+        # find tickets of user for reservation ids
+        for ticket in BookingDBClass.tickets_database:
+            if userId == ticket.user_id:
+                tempAllTicketsResId.append(ticket.reservation_id)
+
+        # get today's date to sort trips in past/current
+        date = str(datetime.datetime.now())
+        date = date.split()[0].split('-')
+        year = int(date[0])
+        month = int(date[1])
+        day = int(date[2])
+        userPast = []
+        userCurrent = []
+        
+        for tr in tempAllTrips:
+            # get reservation container in trips
+            for res in tr.reservation_list:
+                # get reservation from reservation id
+                if res.reservation_id in tempAllTicketsResId:
+                    # get reservation date and check
+                    isPast = False
+                    rd = res.date.split("-")
+                    ryear = rd[0]
+                    rmonth = rd[1]
+                    rday = rd[2]
+                    # if previous year
+                    if ryear < year:
+                        isPast = True
+                    # previous month of this year
+                    elif ryear == year and rmonth < month:
+                        isPast = True
+                    # previous day of this month
+                    elif rmonth == month and rday < day:
+                        isPast = True
+                    
+                    if isPast:
+                        userPast.append(res)
+                    else:
+                        userCurrent.append(res)
+
+        userReservationsOut = [userPast, userCurrent]
+        return userReservationsOut
 
 
--- Trainz/trip.py
+++ Trainz/trip.py
@@ -1,11 +1,15 @@
 class TripContainer: #a trip is a container of reservations. each time you do a booking, you create a Trip "container"
     
+
     def __init__(self, trip_id, user_id): #a trip is like a shopping cart to a user, it should link back to them for better lookup
         self.trip_id = trip_id
-        self.user_id = user_id
+        self.user_id = [user_id] # changed to list so all users on this trip (if family) can access the trip with their own id
         self.reservation_list = [] #trip is a container containing the user's reservations--- list
     
     def add_reservation(self, reservation):
         self.reservation_list.append(reservation)
         print("Reservation added to trip container. remove this print after testing")
 
+    # adding user to trip list so that all users on this trip can access the trip with their id
+    def add_trip_member(self, user_id):
+        self.user_id.append(user_id)
